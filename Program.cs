77538ad40ed3166d3ccb814a8464c19815ab89e2
İlyas Akın <ashnwor@gmail.com>
2025-07-15 01:48:42 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using QuickstartWeatherServer.Tools;
+
+using var host = new HostBuilder()
+    .ConfigureServices(services =>
+    {
+        services.AddMcpServer()
+            .WithStdioServerTransport()
+            .WithTools<GuidTools>();
+    })
+    .Build();
+
+await host.RunAsync();
